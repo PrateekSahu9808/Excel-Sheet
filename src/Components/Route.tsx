6ecc7291fe95c429c6ecc89fcf80d34eff56bece
PrateekSahu9808 <mailprateeksahu@gmail.com>
2024-03-10 22:00:19 +0530

    base

--- src/Components/Route.tsx
+++ src/Components/Route.tsx
@@ -2,27 +2,34 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./Home";
 import Sheet from "./Sheet";
 import Load from "./Load";
-import View from "./View"; // Import the View component
+import View from "./View";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/sheet",
-    element: <Sheet />,
-  },
-  {
-    path: "/load",
-    element: <Load />,
-  },
-  {
-    path: "/view/:key",
-    element: <View />,
-  },
-]);
+const basename = "/Excel-Sheet"; // Replace with your desired base name
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/sheet",
+      element: <Sheet />,
+    },
+    {
+      path: "/load",
+      element: <Load />,
+    },
+    {
+      path: "/view/:key",
+      element: <View />,
+    },
+  ],
+  { basename }
+);
+
 const Route = () => {
   return <RouterProvider router={router} />;
 };
+
 export default Route;
